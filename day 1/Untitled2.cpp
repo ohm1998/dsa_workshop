57f8d487b8e921106734abfde860914cd2ce91e4
ohm1998 <ohm.1998@gmail.com>
2020-08-26 15:50:15 +0530

    day 2

--- day 1/Untitled2.cpp
+++ day 1/Untitled2.cpp
@@ -24,6 +24,10 @@ void insert_node(node **head,int val)
 }
 void display_list(node *head)
 {
+	if(head==NULL)
+	{
+		printf("Empty List");
+	}
 	while(head)
 	{
 		printf("%d ->",head->data);
@@ -37,6 +41,81 @@ void insertion_beggin(node **head,int val)
 	new_node -> next = *head;
 	*head = new_node;
 }
+void insert_pos(node **head,int pos,int val)
+{
+    node *new_node=(node*)malloc(sizeof(node));
+    new_node->data=val;
+    new_node->next=NULL;
+    node *temp;
+    node *p;
+  //  printf("function call");
+    if(pos==1)
+    {
+        if(*head==NULL)
+        {
+            *head=new_node;
+        }
+        else
+		{
+        	new_node -> next = *head;
+			*head = new_node;	
+		}
+    }
+    else{
+    	//printf("here");
+        temp=*head;
+	    for(int i=1;i<pos;i++)
+	    {
+	        p=temp;
+	        //printf("%d",temp->data);
+	        temp=temp->next;
+	        
+	    }
+	    p->next= new_node;
+	    new_node->next=temp;
+    }
+}
+void search(node *head,int ele)
+{
+	int count = 0;
+    while(head)
+	{
+		if(head->data==ele)
+		{
+			printf("Element found at %d \n",count);
+			return;
+		}
+		count++;
+		//printf("%d ->",head->data);
+		head = head->next;
+	}
+	printf("Element Not Found");
+}
+void delete_by_val(node **head,int val)
+{
+	node *curr = *head;
+	node *prev = NULL;
+	while(curr->data != val )
+	{
+		prev = curr;
+		curr = curr->next;	
+	}
+	//printf("%d \n",curr->data);
+	prev->next = curr->next;
+	free(curr);
+}
+void delete_list(node **head)
+{
+	node *curr = *head;
+	node *temp;
+	while(*head)
+	{
+		temp = *head;
+		*head = (*head)->next;
+		free(temp);
+	}
+	//*head = NULL;
+}
 int main()
 {
 	node *head=NULL;
@@ -44,6 +123,12 @@ int main()
 	insert_node(&head,15);
 	insertion_beggin(&head,11);
 	insertion_beggin(&head,9);
+	insert_pos(&head,1,40);
+	//search(head,11);
+	display_list(head);
+	printf("\n\n");
+	delete_by_val(&head,11);
+	delete_list(&head);
 	display_list(head);
 	return 0;
 }
